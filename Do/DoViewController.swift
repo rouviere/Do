65f01bd6277dc9e6e0ffe6890fe439ee0bcf3e7e
rouviere <forrest@rouviere.com>
2018-05-03 14:56:59 -0600

    Add new item functionality complete

--- Do/DoViewController.swift
+++ Do/DoViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class DoViewController: UITableViewController {
   
-  let itemArray = ["Learn Swift", "Find a good job", "Pay off debt"]
+  var itemArray = ["Learn Swift", "Find a good job", "Pay off debt"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   
   }
   
   //MARK - TableView Datasource Methods
-  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return itemArray.count
   }
@@ -31,7 +30,6 @@ class DoViewController: UITableViewController {
   }
   
   //MARK - TableView Delegate Methods
-  
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     print(itemArray[indexPath.row]) // prints the title of each row.
     if let cell = tableView.cellForRow(at: indexPath) {
@@ -42,6 +40,29 @@ class DoViewController: UITableViewController {
       }
       tableView.deselectRow(at: indexPath, animated: true)  // removes the hightlight from the row that is tapped
     }
+  }
+  
+  //MARK - Add New Items
+  @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+    var textField = UITextField() // empty var to be used below
+    
+    let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
+    let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+      // What will happen once the user clickc the Add Item button on our UIAlert?
+      
+      self.itemArray.append(textField.text!)
+      self.tableView.reloadData()
+    }
+    
+    alert.addTextField { (alertTextField) in
+      alertTextField.placeholder = "Create new item"
+      textField = alertTextField
+    }
+    
+    alert.addAction(action)
+    
+    present(alert, animated: true, completion: nil)
   }
 }
 
